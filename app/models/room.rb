f54821569bcdb2a4da2a7481a9caaccf6481db94
AF <af.arthurfaria@gmail.com>
2026-01-22 13:03:47 -0300

    feat(room): generating admin token and slug

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,11 @@
 class Room < ApplicationRecord
+  has_many :issues, dependent: :destroy
+  before_create :set_slug_and_token
+
+  private
+
+  def set_slug_and_token
+    self.slug = SecureRandom.alphanumeric(6).downcase
+    self.admin_token = SecureRandom.hex(16)
+  end
 end
